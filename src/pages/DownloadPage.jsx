29f817d81c742c4d2de8028ff50c76da5d3e3ae2
Trenton Kress <trenton.kress@gmail.com>
2023-05-28 22:10:40 -0700

    Fix download links

--- src/pages/DownloadPage.jsx
+++ src/pages/DownloadPage.jsx
@@ -10,10 +10,13 @@ export default function DownloadPage() {
                 </div>
                 <div className="button-container">
                     <div>
-                        <button className="download-client1 button-hover"><a href="DarkanSetup.msi" download><h2>Windows Installer</h2></a></button>
+                        <button className="download-client1 button-hover"><a href="https://github.com/DarkanRS/client-loader/releases/download/1.0.1/DarkanSetup.msi" download><h2>Windows Installer</h2></a></button>
                     </div>
                     <div>
-                        <button className="download-client2 button-hover"><a href="Darkan.jar" download><h2>Java 17 Jar</h2></a></button>
+                        <button className="download-client2 button-hover"><a href="https://github.com/DarkanRS/client-loader/releases/download/1.0.1/Darkan.jar" download><h2>Java 17 Jar</h2></a></button>
+                    </div>
+                    <div>
+                        <button className="download-client3 button-hover"><a href="https://gitlab.com/darkanrs/client-loader/-/releases" download><h2>Other (Github Releases)</h2></a></button>
                     </div>
                 </div>
             </div>
